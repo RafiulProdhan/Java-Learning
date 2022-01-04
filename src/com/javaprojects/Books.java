005e2065fca6729b816f94640ba56fa188f7fa45
Rafiul <rafiulrprodhan@gmail.com>
2022-01-04 13:12:52 -0500

    Update

--- src/com/javaprojects/Books.java
+++ src/com/javaprojects/Books.java
@@ -3,6 +3,9 @@ package com.javaprojects;
 public class Books {
     public static void main(String[] args) {
 
-        Books book1 = new BookConstructor("Harry Potter, JK Rowling, 500, "English");
+        Books book1 = new BookConstructor("Harry Potter, "JK Rowling", 500, "English");
+
     }
 }
+
+
